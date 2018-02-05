54894aaea81c76738181c0c58ec39ba953390197
Alex Anthony <alex19427@gmail.com>
2018-02-05 17:44:00 +0530

    Update build.py

--- q05_split/build.py
+++ q05_split/build.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from sklearn.model_selection import train_test_split
-from q03_encode_features.build import q03_encode_features
+from q03_enc_features.build import q03_encode_features
 path = 'data/GermanData.csv'
 
 def q05_split():
