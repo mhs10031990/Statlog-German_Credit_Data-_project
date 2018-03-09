d58002473d5dc5af08bf749b253cea406711f41b
Alex Anthony <alex19427@gmail.com>
2018-03-09 15:53:26 +0530

    Update build.py

--- q05_split/build.py
+++ q05_split/build.py
@@ -1,6 +1,6 @@
 import sys, os
 from sklearn.model_selection import train_test_split
-from greyatomlib.statlog_german_gredit_data_project.q03_encode_features.build import q03_encode_features
+from greyatomlib.statlog_german_credit_data_project.q03_encode_features.build import q03_encode_features
 path = 'data/GermanData.csv'
 
 def q05_split():
