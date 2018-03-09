699e38ad14f52cee97844d23e35caa6e761623b0
Alex Anthony <alex19427@gmail.com>
2018-03-09 15:52:39 +0530

    Update build.py

--- q03_enc_features/build.py
+++ q03_enc_features/build.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import sys, os
 from sklearn.preprocessing import LabelEncoder
-from q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
+from greyatomlib.statlog_german_credit_data_project.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 
 path = 'data/GermanData.csv'
 
