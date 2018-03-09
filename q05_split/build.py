bdd6a12dc6a6c97b14382f17e6a8396dc0be5025
Alex Anthony <alex19427@gmail.com>
2018-03-09 15:47:20 +0530

    Update build.py

--- q05_split/build.py
+++ q05_split/build.py
@@ -1,7 +1,6 @@
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from sklearn.model_selection import train_test_split
-from q03_enc_features.build import q03_encode_features
+from greyatomlib.statlog_german_gredit_data_project.q03_encode_features.build import q03_encode_features
 path = 'data/GermanData.csv'
 
 def q05_split():
