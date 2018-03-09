24c2f36bcfa9055f9ad037af77201785070970d6
Alex Anthony <alex19427@gmail.com>
2018-03-09 15:51:52 +0530

    Update build.py

--- q02_plot_all_variable/build.py
+++ q02_plot_all_variable/build.py
@@ -4,7 +4,7 @@ import sys, os
 from math import ceil
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
-from greyatomlib.statlog_german_gredit_data_project.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
+from greyatomlib.statlog_german_credit_data_project.q01_load_data_and_add_column_names.build import q01_load_data_and_add_column_names
 
 path = 'data/GermanData.csv'
 
