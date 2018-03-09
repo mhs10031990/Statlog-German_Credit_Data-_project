338b2003ef9fbc621b838fe0c936977bb7e2660f
Alex Anthony <alex19427@gmail.com>
2018-03-09 15:54:22 +0530

    Update build.py

--- q07_randomsearch_predict/build.py
+++ q07_randomsearch_predict/build.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import roc_auc_score
-from q06_feature_preprocessing.build import q06_feature_preprocessing
+from greyatomlib.statlog_german_credit_data_project.q06_feature_preprocessing.build import q06_feature_preprocessing
 
 path = 'data/GermanData.csv'
 
