183b47228cc85f6830ab7b700cac69401d1d7889
Alex Anthony <alex19427@gmail.com>
2018-03-09 15:45:54 +0530

    Update build.py

--- q04_correlation_plot/build.py
+++ q04_correlation_plot/build.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 plt.switch_backend('agg')
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from q03_enc_features.build import q03_encode_features
 path = 'data/GermanData.csv'
 
